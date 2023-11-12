00b04a14d65573c8af9fc88165d911c5faf567e7
CrashStark <starkcrash0@gmail.com>
2023-11-12 14:46:44 +0530

    Add URL event

--- Angular-Ecommerce/Ecomm-project/src/app/header/header.component.ts
+++ Angular-Ecommerce/Ecomm-project/src/app/header/header.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  menuType='default';
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    
+    this.router.events.subscribe((event:any)=>{
+      if(event.url){
+
+        if(localStorage.getItem("seller")&&event.url.includes('seller')){
+          console.log("In Seller Area");
+          this.menuType ='seller';
+        }else{
+          console.log("outside Seller");
+          this.menuType='default';
+        }
+        console.log(event.url);
+      }
+     
+    })
   }
 
 }
