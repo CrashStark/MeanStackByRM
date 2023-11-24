cff3d1fe22fcba37118df7647ee721d2c387566b
CrashStark <starkcrash0@gmail.com>
2023-11-24 14:53:28 +0530

    created Header in webstore

--- WebshopProjectwithStripe/webshop/src/app/app.module.ts
+++ WebshopProjectwithStripe/webshop/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { MatBadgeModule, MatButtonModule, MatCardModule, MatExpansionModule, MatGridListModule, MatIconModule, MatListModule, MatMenuModule, MatSidenav, MatSidenavModule, MatSnackBarModule, MatTableModule, MatToolbarModule } from "@angular/material";
 
+import { HeaderComponent } from "./component/header/header.component";
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTableModule } from "@angular/material/table";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,6 +36,7 @@ import { MatBadgeModule, MatButtonModule, MatCardModule, MatExpansionModule, Mat
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
