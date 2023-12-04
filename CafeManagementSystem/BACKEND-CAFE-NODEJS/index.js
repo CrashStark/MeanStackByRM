e7c384d26f460bcfd74c5e49b0c8ac3424c1c6cf
CrashStark <starkcrash0@gmail.com>
2023-12-05 00:16:09 +0530

    Category Api created

--- CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
@@ -2,9 +2,11 @@ const express=require('express');
 var cors=require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
+const categoryroutes=require('./routes/category')
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use('/user',userRoute);
+app.use('/category',categoryroutes);
 module.exports=app;
--- CafeManagementSystem/BACKEND-CAFE-NODEJS/table.sql
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/table.sql
@@ -11,3 +11,8 @@ create table user(
 
 insert into user(name,contactNumber,email,password,status,role)
 values('Admin','1212121212','admin@gmail.com','admin','true','admin');
+
+create table category(
+    id int NOT NULL AUTO_INCREMENT
+    name varchar(255) NOT NULL,
+    primary key(id));
