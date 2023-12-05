e3f3243d909c70e36a24b3f730d177577007d675
CrashStark <starkcrash0@gmail.com>
2023-12-05 14:53:37 +0530

    Added Product API

--- CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
@@ -2,11 +2,13 @@ const express=require('express');
 var cors=require('cors');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
-const categoryroutes=require('./routes/category')
+const categoryroutes=require('./routes/category');
+const productRoute=require('./routes/product');
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use('/user',userRoute);
 app.use('/category',categoryroutes);
+app.use('/product',productRoute);
 module.exports=app;
