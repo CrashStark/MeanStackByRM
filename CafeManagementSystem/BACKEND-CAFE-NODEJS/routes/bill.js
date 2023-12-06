143a9b427ecd5784fd48287ff4e8f8777b6b88f4
CrashStark <starkcrash0@gmail.com>
2023-12-06 15:36:42 +0530

    Added Generation of pdf api

--- CafeManagementSystem/BACKEND-CAFE-NODEJS/routes/bill.js
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/routes/bill.js
@@ -62,4 +62,40 @@ router.post("/generateReport", auth.authenticateToken, (req, res) => {
   );
 });
 
+router.post('/getPdf',auth.authenticateToken,function(req,res){
+  const orderDetails = req.body;
+  const pdfPath ='./generated_pdf/'+orderDetails.uuid+'.pdf';
+  if(fs.existsSync(pdfPath)){
+    res.contentType("application/pdf");
+    fs.createReadStream(pdfPath).pipe(res);
+  }else{
+    var productDetailsReport = JSON.parse(orderDetails.productDetails);
+    ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
+      productDetails: productDetailsReport,
+      name: orderDetails.name,
+      email: orderDetails.email,
+      contactNumber: orderDetails.contactNumber,
+      paymentMethod: orderDetails.paymentMethod,
+      totalAmount: orderDetails.totalAmount,
+    },(err,html)=>{
+        if(err){
+            return res.status(500).json(err+"There is some issue");
+        }else{
+            pdf.create(html).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf",function(err,results){
+                if(err){
+                    console.log(err);
+                    return res.status(500).json(er);
+                }else{
+                  res.contentType("application/pdf");
+                  fs.createReadStream(pdfPath).pipe(res);
+                }
+            });
+        }
+    });
+  }
+})
+
+router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
+  var query='select * from bill order by id DESC';
+})
 module.exports=router;
