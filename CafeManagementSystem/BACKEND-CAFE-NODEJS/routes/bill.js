a758ff78904cdb4b47413fcd8e3d3a3518a50300
CrashStark <starkcrash0@gmail.com>
2023-12-07 14:13:44 +0530

    All the bill api is completed

--- CafeManagementSystem/BACKEND-CAFE-NODEJS/routes/bill.js
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/routes/bill.js
@@ -34,27 +34,36 @@ router.post("/generateReport", auth.authenticateToken, (req, res) => {
     ],
     (err, results) => {
       if (!err) {
-        ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
-          productDetails: productDetailsReport,
-          name: orderDetails.name,
-          email: orderDetails.email,
-          contactNumber: orderDetails.contactNumber,
-          paymentMethod: orderDetails.paymentMethod,
-          totalAmount: orderDetails.totalAmount,
-        },(err,html)=>{
-            if(err){
-                return res.status(500).json(err+"There is some issue");
-            }else{
-                pdf.create(html).toFile('./generated_pdf/' + generateUuid + ".pdf",function(err,results){
-                    if(err){
-                        console.log(err);
-                        return res.status(500).json(er);
-                    }else{
-                        return res.status(200).json({uuid:generateUuid});
+        ejs.renderFile(
+          path.join(__dirname, "", "report.ejs"),
+          {
+            productDetails: productDetailsReport,
+            name: orderDetails.name,
+            email: orderDetails.email,
+            contactNumber: orderDetails.contactNumber,
+            paymentMethod: orderDetails.paymentMethod,
+            totalAmount: orderDetails.totalAmount,
+          },
+          (err, html) => {
+            if (err) {
+              return res.status(500).json(err + "There is some issue");
+            } else {
+              pdf
+                .create(html)
+                .toFile(
+                  "./generated_pdf/" + generateUuid + ".pdf",
+                  function (err, results) {
+                    if (err) {
+                      console.log(err);
+                      return res.status(500).json(er);
+                    } else {
+                      return res.status(200).json({ uuid: generateUuid });
                     }
-                });
+                  }
+                );
             }
-        });
+          }
+        );
       } else {
         return res.status(500).json(err);
       }
@@ -62,40 +71,71 @@ router.post("/generateReport", auth.authenticateToken, (req, res) => {
   );
 });
 
-router.post('/getPdf',auth.authenticateToken,function(req,res){
+router.post("/getPdf", auth.authenticateToken, function (req, res) {
   const orderDetails = req.body;
-  const pdfPath ='./generated_pdf/'+orderDetails.uuid+'.pdf';
-  if(fs.existsSync(pdfPath)){
+  const pdfPath = "./generated_pdf/" + orderDetails.uuid + ".pdf";
+  if (fs.existsSync(pdfPath)) {
     res.contentType("application/pdf");
     fs.createReadStream(pdfPath).pipe(res);
-  }else{
+  } else {
     var productDetailsReport = JSON.parse(orderDetails.productDetails);
-    ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
-      productDetails: productDetailsReport,
-      name: orderDetails.name,
-      email: orderDetails.email,
-      contactNumber: orderDetails.contactNumber,
-      paymentMethod: orderDetails.paymentMethod,
-      totalAmount: orderDetails.totalAmount,
-    },(err,html)=>{
-        if(err){
-            return res.status(500).json(err+"There is some issue");
-        }else{
-            pdf.create(html).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf",function(err,results){
-                if(err){
-                    console.log(err);
-                    return res.status(500).json(er);
-                }else{
+    ejs.renderFile(
+      path.join(__dirname, "", "report.ejs"),
+      {
+        productDetails: productDetailsReport,
+        name: orderDetails.name,
+        email: orderDetails.email,
+        contactNumber: orderDetails.contactNumber,
+        paymentMethod: orderDetails.paymentMethod,
+        totalAmount: orderDetails.totalAmount,
+      },
+      (err, html) => {
+        if (err) {
+          return res.status(500).json(err + "There is some issue");
+        } else {
+          pdf
+            .create(html)
+            .toFile(
+              "./generated_pdf/" + orderDetails.uuid + ".pdf",
+              function (err, results) {
+                if (err) {
+                  console.log(err);
+                  return res.status(500).json(er);
+                } else {
                   res.contentType("application/pdf");
                   fs.createReadStream(pdfPath).pipe(res);
                 }
-            });
+              }
+            );
         }
-    });
+      }
+    );
   }
-})
+});
 
-router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
-  var query='select * from bill order by id DESC';
-})
-module.exports=router;
+router.get("/getBills", auth.authenticateToken, (req, res, next) => {
+  var query = "select * from bill order by id DESC";
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json(results);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.delete("/delete/:id", auth.authenticateToken, (req, res, next) => {
+  const id = req.params.id;
+  var query = "delete from bill where id =?";
+  connection.query(query, [id], (err, results) => {
+    if (!err) {
+      if (results.affectedRows == 0) {
+        return res.status(404).json({ message: "Bill Id does not found" });
+      }
+      return res.status(200).json({ message: "Bill Deleted Successfully" });
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+module.exports = router;
