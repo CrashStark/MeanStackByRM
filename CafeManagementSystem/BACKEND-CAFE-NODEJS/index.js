fd1b7996a4295147677de26c3e434f56e962f7cb
CrashStark <starkcrash0@gmail.com>
2023-12-07 19:49:54 +0530

    dashbord completed

--- CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
+++ CafeManagementSystem/BACKEND-CAFE-NODEJS/index.js
@@ -5,6 +5,7 @@ const userRoute = require('./routes/user');
 const categoryroutes=require('./routes/category');
 const productRoute=require('./routes/product');
 const billRoute = require('./routes/bill');
+const dashBoardRoute = require('./routes/dashboard');
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
@@ -12,6 +13,6 @@ app.use(express.json());
 app.use('/user',userRoute);
 app.use('/category',categoryroutes);
 app.use('/product',productRoute);
-
+app.use('/dashboard',dashBoardRoute);
 app.use('/bill',billRoute);
 module.exports=app;
